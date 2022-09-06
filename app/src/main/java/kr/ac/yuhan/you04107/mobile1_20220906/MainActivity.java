037e80ba026c42d26c93ba5a38a96f9bd931283c
you04107 <you04107@naver.com>
2022-09-06 16:37:40 +0900

    유한대학교

--- app/src/main/java/kr/ac/yuhan/you04107/mobile1_20220906/MainActivity.java
+++ app/src/main/java/kr/ac/yuhan/you04107/mobile1_20220906/MainActivity.java
@@ -3,6 +3,9 @@ package kr.ac.yuhan.you04107.mobile1_20220906;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button btn = findViewById(R.id.but);
+        //  Anonymous class(익명클래스) 객체
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Toast t = Toast.makeText(MainActivity.this,
+                            "내일은 9월 7일 수요일입니다.", Toast.LENGTH_LONG);
+                t.show();
+          }
+    });
+}
+
 }
